4d944cfe6f6ddd736c96b49e18057b2bfea2eb16
Hưng Vũ Tuấn <hungvt@acbs.com.vn>
2026-04-29 03:45:05 +0000

    Thêm sheet_non_business_day để lấy danh sách ngày nghỉ

--- definitions/external_sources.js
+++ definitions/external_sources.js
@@ -2,4 +2,5 @@ declare({schema: 'bronze', name: "CORTCUSTOMER"});
 declare({schema: 'bronze', name: "CORTCASH_MOVEMENT_HISTORY"});
 declare({schema: 'bronze', name: "CORTSUB_ACCOUNT"});
 declare({schema: 'bronze', name: "CORTASSET_EXT"}),
-declare({schema: 'bronze', name: "BRKTBROKER"})
+declare({schema: 'bronze', name: "BRKTBROKER"}),
+declare({schema: 'bronze', name: "sheet_non_business_day"})
