0345f94c577047e7a8ad1360a1436e5dd5b9fc89
Hưng Vũ Tuấn <hungvt@acbs.com.vn>
2026-05-20 04:40:39 +0000

    Thêm luồng xử lý CORTSEC_BALANCE_HISTORY

--- definitions/external_sources.js
+++ definitions/external_sources.js
@@ -7,4 +7,5 @@ declare({schema:"bronze",name:"CORTASSET"}),
 declare({schema:"bronze",name:"MRGTDEBIT_CONTRACT_HIST"}),
 declare({schema:"bronze",name:"MSTTBRANCH"}),
 declare({schema:"bronze",name:"MSTTMARKET"}),
+declare({schema:"bronze",name:"CORTSEC_BALANCE_HISTORY"}),
 declare({schema:"bronze",name:"MSTTSECURITIES"})
