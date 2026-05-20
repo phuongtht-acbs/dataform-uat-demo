ece878a17cef5e1fea8ac9c67d110e7ce38a616a
Hưng Vũ Tuấn <hungvt@acbs.com.vn>
2026-05-20 10:59:13 +0000

    Thêm luồng xử lý RHTTRIGHT_INFO

--- definitions/external_sources.js
+++ definitions/external_sources.js
@@ -8,5 +8,6 @@ declare({schema:"bronze",name:"MRGTDEBIT_CONTRACT_HIST"}),
 declare({schema:"bronze",name:"MSTTBRANCH"}),
 declare({schema:"bronze",name:"MSTTMARKET"}),
 declare({schema:"bronze",name:"CORTSEC_BALANCE_HISTORY"}),
-declare({ schema: "bronze", name: "ORDTORDER_HIST" });
+declare({ schema: "bronze", name: "ORDTORDER_HIST" }),
+declare({ schema: "bronze", name: "RHTTRIGHT_INFO" }),
 declare({schema:"bronze",name:"MSTTSECURITIES"})
